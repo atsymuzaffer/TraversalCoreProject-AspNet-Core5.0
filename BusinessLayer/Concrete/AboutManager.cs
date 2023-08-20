58879580bced81270a75c89134b88aa89cfaf2de
Muzaffer Atasoy <a_muzafferr@hotmail.com>
2023-08-20 13:02:04 +0300

    Developing Process V1

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        private IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+        public void TAdd(About t)
+        {
+            _aboutDal.Insert(t);
+
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutDal.Delete(t);
+
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.Update(t);
+        }
+
+        public List<About> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        //buraya gelince about t de hata olabilir kontrol et
+       // public List<About> TGetList(About t)
+        //{
+         //   return _aboutDal.GetList();
+        //}
+
+        public About TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/DestinationManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class DestinationManager : IDestinationService
+    {
+
+        private IDestinationDal _destinationDal;
+
+        public DestinationManager(IDestinationDal destinationDal)
+        {
+            _destinationDal = destinationDal;
+        }
+
+        public void TAdd(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Destination> TGetList()
+        {
+            return _destinationDal.GetList();
+        }
+
+        public Destination TGetByID(int id)
+        {
+
+            return _destinationDal.GetById(id);
+
+        }
+    }
+}
